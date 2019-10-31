2e1ec9d42a12c55d6ef227c3ec896e70f75c9526
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-31 17:19:58 +0000

    login page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,81 @@
 import React, { Component } from "react";
-import hash from "./hash.js"
+import hash from "./hash.js";
 import logo from "./logo.svg";
 import "./App.css";
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const clientId = "726111e1671c41098283a125c32be806";
-const redirectUri = "http://localhost:3000";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
-
+import { authEndpoint, clientId, redirectUri, scopes } from "./config.js";
+import Player from "./Player";
+import * as $ from "jquery";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms: 0
+      },
+      is_playing: "Paused",
+      progress_ms: 0
+    };
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
   componentDidMount() {
-
     let _token = hash.access_token;
     if (_token) {
-
       this.setState({
         token: _token
       });
+      this.getCurrentlyPlaying(_token);
     }
   }
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {!this.state.token && (
-        <a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-        >
-          Login to Spotify
-        </a>
-      )}
-      {this.state.token && (
-        // Spotify Player Will Go Here In the Next Step
-      )}
-      </header>
-    </div>
-  );
+
+  getCurrentlyPlaying(token) {
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: data => {
+        console.log("data", data);
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Log In
+            </a>
+          )}
+          {this.state.token && (
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.progress_ms}
+            />
+          )}
+        </header>
+      </div>
+    );
   }
 }
 export default App;
