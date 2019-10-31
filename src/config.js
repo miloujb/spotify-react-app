f0e95c62dacd0a34729373bac1ba28ebe6cf5b29
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-31 17:27:29 +0000

    gitignore

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-
-export const clientId = "726111e1671c41098283a125c32be806";
-export const redirectUri = "http://localhost:3000/redirect";
-export const scopes = [
-  "user-top-read",
-  "user-read-currently-playing",
-  "user-read-playback-state"
-];
